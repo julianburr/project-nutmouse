58bb734d202456d526607e96822fc2c044d4163a
julianburr <hello@julianburr.de>
2015-07-04 19:17:11 +0800

    NEW user model class

---
+++ classes/Model/User.php
@@ -0,0 +1,71 @@
+<?php
+
+class User {
+	
+	private $id = null;
+	
+	private $data = array();
+	
+	public function __construct($id=null){
+		if(!is_null($id)){
+			// If id is given, load user from id
+			$this->load($id);
+		}
+	}
+	
+	public function load($id){
+		// Load user from database
+		$sql = new SqlManager();
+		$sql->setQuery("SELECT * FROM user WHERE id = {{id}}");
+		$sql->bindParam("{{id}}", $id);
+		$user = $sql->result();
+		if(isset($user['id']) && $user['id'] == $id){
+			$this->id = $id;
+			$this->data = $user;
+		}
+	}
+	
+	public function loadByLogin($username, $password){
+		// Load user from database by given login data
+		$sql = new SqlManager();
+		$sql->setQuery("SELECT id FROM user WHERE username = '{{username}}' AND password = '{{password}}'");
+		$sql->bindParam("{{username}}", $username);
+		$sql->bindParam("{{password}}", md5($password));
+		$user = $sql->result();
+		if(isset($user['id'])){
+			$this->load($user['id']);
+		}
+	}
+	
+	public function update(array $data){
+		// Update user in database from given data array
+		$sql = new SqlManager();
+		$sql->update("user", $data);
+	}
+	
+	public function delete(array $data){
+		// Delete user from data array
+		$sql = new SqlManager();
+		$sql->delete("user", $data);
+	}
+	
+	public function deleteByID(int $id){
+		// Delete user by from given id
+		$data = array("id" => $id);
+		$this->delete($data);
+	}
+	
+	public function create(array $data){
+		// Create new user from given data array
+		$sql = new SqlManager();
+		$sql->insert("user", $data);
+		// Return database ID of added user
+		return $sql->getLastInsertID();
+	}
+	
+	public function getID(){
+		// Get current users ID (null if no user has been loaded!)
+		return $this->id;
+	}
+	
+}
