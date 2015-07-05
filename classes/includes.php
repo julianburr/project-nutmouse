525e78c469960ddeba1accd5d67687246ffce3c4
julianburr <hello@julianburr.de>
2015-07-05 12:32:04 +0800

    UPD include new model classes

--- classes/includes.php
+++ classes/includes.php
@@ -10,6 +10,10 @@ include_once(__DIR__ . "/System/Session.php");
 include_once(__DIR__ . "/System/Config.php");
 include_once(__DIR__ . "/System/Template.php");
 
+// Model classes
+include_once(__DIR__ . "/Model/Content.php");
+include_once(__DIR__ . "/Model/User.php");
+
 // Utilities e.g. for database interactions
 include_once(__DIR__ . "/Utils/SqlManager.php");
 include_once(__DIR__ . "/Utils/DateManager.php");
