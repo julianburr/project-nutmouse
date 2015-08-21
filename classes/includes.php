810a94901bcf64f8a578942210088a9cb2f94523
julianburr <hello@julianburr.de>
2015-08-21 20:55:57 +0800

    ADD all new classes

--- classes/includes.php
+++ classes/includes.php
@@ -9,6 +9,12 @@ include_once(__DIR__ . "/Controller.php");
 include_once(__DIR__ . "/System/Session.php");
 include_once(__DIR__ . "/System/Config.php");
 include_once(__DIR__ . "/System/Template.php");
+include_once(__DIR__ . "/System/Action.php");
+include_once(__DIR__ . "/System/HookPoint.php");
+include_once(__DIR__ . "/System/Plugin.php");
+include_once(__DIR__ . "/System/Meta.php");
+include_once(__DIR__ . "/System/Cache.php");
+include_once(__DIR__ . "/System/Locale.php");
 
 // Model classes
 include_once(__DIR__ . "/Model/Content.php");
@@ -17,3 +23,4 @@ include_once(__DIR__ . "/Model/User.php");
 // Utilities e.g. for database interactions
 include_once(__DIR__ . "/Utils/SqlManager.php");
 include_once(__DIR__ . "/Utils/DateManager.php");
+include_once(__DIR__ . "/Utils/FileManager.php");
