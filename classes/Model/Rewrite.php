28f5dcdeb9933c1435f6bbb73fd6f7efb7846849
julianburr <hello@julianburr.de>
2015-09-05 14:42:22 +0800

    NEW class for handling of rewrite rules

---
+++ classes/Model/Rewrite.php
@@ -0,0 +1,45 @@
+<?php
+
+class Rewrite {
+	
+	private $request_url = null;
+	private $target_url = null;
+	
+	private $id = null;
+	private $data = array();
+	
+	public function __construct($url=null){
+		$this->setUrl($url);
+	}
+	
+	public function getTargetUrl(){
+		return $this->target_url;
+	}
+	
+	public function setUrl($url){
+		$this->request_url = $url;
+		$this->target_url = $url;
+	}
+	
+	public function applyRules(){
+		$sql = new SqlManager();
+		$sql->setQuery("
+			SELECT * FROM rewrite
+			WHERE request = '{{url}}'
+			");
+		$sql->bindParam("{{url}}", $this->request_url);
+		$rule = $sql->result();
+		if(isset($rule['rewrite'])){
+			$this->target_url = $rule['rewrite'];
+			$this->data = $rule;
+		}
+	}
+	
+	public function isRedirect(){
+		if(isset($this->data['redirect']) && $this->data['redirect'] == 1){
+			return true;
+		}
+		return false;
+	}
+	
+}
